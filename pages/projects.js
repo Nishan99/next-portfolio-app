ca386cdd289b84f7fcb2909862e924906fb00197
unknown <nishanpaudel305@gmail.com>
2023-06-26 18:08:17 +0545

    update::open project in a new tab

--- pages/projects.js
+++ pages/projects.js
@@ -1,119 +1,148 @@
-import styles from '../styles/Project.module.css'
+import styles from "../styles/Project.module.css";
 import { AiFillHtml5 } from "react-icons/ai";
 import { IoLogoJavascript } from "react-icons/io";
-import { DiCss3} from "react-icons/di";
-import {  FaReact } from "react-icons/fa";
-import Link from 'next/link'
-const Projects=()=>{
-  return(
-    <div className={styles.project} id='project'>
-        <h1>Projects</h1>
-        <div className={styles.project_main}>
-          <div className={styles.project_each}>
-            <h3>CarDetails | CarHub</h3>
-            <p className={styles.project_desc}>Cars list with their details...</p>
-              <p className={styles.project_title}>Technologies</p>
-            <div className={styles.project_icons}>
+import { DiCss3 } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import Link from "next/link";
+const Projects = () => {
+  return (
+    <div className={styles.project} id="project">
+      <h1>Projects</h1>
+      <div className={styles.project_main}>
+        <div className={styles.project_each}>
+          <h3>CarDetails | CarHub</h3>
+          <p className={styles.project_desc}>Cars list with their details...</p>
+          <p className={styles.project_title}>Technologies</p>
+          <div className={styles.project_icons}>
             <li>
-            <AiFillHtml5 size="40px" className={styles.tech_icon} />
-            HTML5
-          </li>
-          <li>
-            <DiCss3 size="40px" className={styles.tech_icon} />
-            CSS3
-          </li>
-          <li>
-            <IoLogoJavascript size="40px" className={styles.tech_icon} />
-            Javascript
-          </li>
-          <li>
-            <FaReact size="40px" className={styles.tech_icon} />
-            Next.js
-          </li>
-            </div>
-            <Link href="https://next-carhub.vercel.app/">
-            <button className={styles.project_btn}>View Project</button>
-            </Link>
-          </div>
-          <div className={styles.project_each}>
-            <h3>Dashboard Panel</h3>
-            <p className={styles.project_desc}>Admin Panel where detail information is deplayed</p>
-              <p className={styles.project_title}>Technologies</p>
-            <div className={styles.project_icons}>
+              <AiFillHtml5 size="40px" className={styles.tech_icon} />
+              HTML5
+            </li>
             <li>
-            <AiFillHtml5 size="40px" className={styles.tech_icon} />
-            HTML5
-          </li>
-          <li>
-            <DiCss3 size="40px" className={styles.tech_icon} />
-            CSS3
-          </li>
-          <li>
-            <IoLogoJavascript size="40px" className={styles.tech_icon} />
-            Javascript
-          </li>
-          <li>
-            <FaReact size="40px" className={styles.tech_icon} />
-            React.js
-          </li>
-            </div>
-            <Link href="https://preeminent-tanuki-286df7.netlify.app/">
-            <button className={styles.project_btn}>View Project</button>
-            </Link>
-          </div>
-          <div className={styles.project_each}>
-            <h3>Product Website</h3>
-            <p className={styles.project_desc}>Single Landing web page to showcase product information</p>
-              <p className={styles.project_title}>Technologies</p>
-            <div className={styles.project_icons}>
+              <DiCss3 size="40px" className={styles.tech_icon} />
+              CSS3
+            </li>
             <li>
-            <AiFillHtml5 size="40px" className={styles.tech_icon} />
-            HTML5
-          </li>
-          <li>
-            <DiCss3 size="40px" className={styles.tech_icon} />
-            CSS3
-          </li>
-          <li>
-            <IoLogoJavascript size="40px" className={styles.tech_icon} />
-            Javascript
-          </li>
-          <li>
-            <FaReact size="40px" className={styles.tech_icon} />
-            React.js
-          </li>
-            </div>
-            <Link href="https://business-project.netlify.app/">
-            <button className={styles.project_btn}>View Project</button>
-            </Link>
-          </div>
-          <div className={styles.project_each}>
-            <h3>Travel Website</h3>
-            <p className={styles.project_desc}>Travel website </p>
-              <p className={styles.project_title}>Technologies</p>
-            <div className={styles.project_icons}>
+              <IoLogoJavascript size="40px" className={styles.tech_icon} />
+              Javascript
+            </li>
             <li>
-            <AiFillHtml5 size="40px" className={styles.tech_icon} />
-            HTML5
-          </li>
-          <li>
-            <DiCss3 size="40px" className={styles.tech_icon} />
-            CSS3
-          </li>
-          <li>
-            <IoLogoJavascript size="40px" className={styles.tech_icon} />
-            Javascript
-          </li>
-         
-            </div>
-            <Link href="https://upbeat-shannon-25ad23.netlify.app/">
-            <button className={styles.project_btn}>View Project</button>
-            </Link>
+              <FaReact size="40px" className={styles.tech_icon} />
+              Next.js
+            </li>
           </div>
+          <Link href="https://next-carhub.vercel.app/" passHref>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://next-carhub.vercel.app/"
+            >
+              <button className={styles.project_btn}>View Project</button>
+            </a>
+          </Link>
+        </div>
+        <div className={styles.project_each}>
+          <h3>Dashboard Panel</h3>
+          <p className={styles.project_desc}>
+            Admin Panel where detail information is deplayed
+          </p>
+          <p className={styles.project_title}>Technologies</p>
+          <div className={styles.project_icons}>
+            <li>
+              <AiFillHtml5 size="40px" className={styles.tech_icon} />
+              HTML5
+            </li>
+            <li>
+              <DiCss3 size="40px" className={styles.tech_icon} />
+              CSS3
+            </li>
+            <li>
+              <IoLogoJavascript size="40px" className={styles.tech_icon} />
+              Javascript
+            </li>
+            <li>
+              <FaReact size="40px" className={styles.tech_icon} />
+              React.js
+            </li>
+          </div>
+          <Link href="https://preeminent-tanuki-286df7.netlify.app/" passHref>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://preeminent-tanuki-286df7.netlify.app/"
+            >
+              <button className={styles.project_btn}>View Project</button>
+            </a>
+          </Link>
+        </div>
+        <div className={styles.project_each}>
+          <h3>Product Website</h3>
+          <p className={styles.project_desc}>
+            Single Landing web page to showcase product information
+          </p>
+          <p className={styles.project_title}>Technologies</p>
+          <div className={styles.project_icons}>
+            <li>
+              <AiFillHtml5 size="40px" className={styles.tech_icon} />
+              HTML5
+            </li>
+            <li>
+              <DiCss3 size="40px" className={styles.tech_icon} />
+              CSS3
+            </li>
+            <li>
+              <IoLogoJavascript size="40px" className={styles.tech_icon} />
+              Javascript
+            </li>
+            <li>
+              <FaReact size="40px" className={styles.tech_icon} />
+              React.js
+            </li>
+          </div>
+          <Link href="https://business-project.netlify.app/" passHref>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://business-project.netlify.app/"
+            >
+              <button className={styles.project_btn}>View Project</button>
+            </a>
+          </Link>
+
+          
+        </div>
+        <div className={styles.project_each}>
+          <h3>Travel Website</h3>
+          <p className={styles.project_desc}>Travel website </p>
+          <p className={styles.project_title}>Technologies</p>
+          <div className={styles.project_icons}>
+            <li>
+              <AiFillHtml5 size="40px" className={styles.tech_icon} />
+              HTML5
+            </li>
+            <li>
+              <DiCss3 size="40px" className={styles.tech_icon} />
+              CSS3
+            </li>
+            <li>
+              <IoLogoJavascript size="40px" className={styles.tech_icon} />
+              Javascript
+            </li>
+          </div>
+          <Link href="https://upbeat-shannon-25ad23.netlify.app/" passHref>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://upbeat-shannon-25ad23.netlify.app/"
+            >
+              <button className={styles.project_btn}>View Project</button>
+            </a>
+          </Link>
          
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
